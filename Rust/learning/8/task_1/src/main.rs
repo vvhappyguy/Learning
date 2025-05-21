50063c7d9f47d5a813cb1d37300142fae7c0bb81
Ivan Dyakin <vvhappyguy@gmail.com>
2025-05-21 10:11:59 +0400

    added description for task 1

--- Rust/learning/8/task_1/src/main.rs
+++ Rust/learning/8/task_1/src/main.rs
@@ -1,3 +1,7 @@
+// Given a list of integers, use a vector 
+// and return the median (when sorted, the value in the middle position) 
+// and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+
 fn create_vec(len: i32, min_rnd: i32, max_rnd: i32) -> Vec<i32>
 {
     use rand::Rng;
